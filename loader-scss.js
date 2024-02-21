13146a1d0574e9fd5d95cb857b9d557cd4b80842
Anastasia Kurochkina <kuronastya14@yandex.ru>
2024-02-22 00:57:11 +0400

    fix tabs

--- loader-scss.js
+++ loader-scss.js
@@ -1,22 +1,21 @@
-
 export async function resolve(specifier, context, next) {
-	const nextResult = await next(specifier, context);
+  const nextResult = await next(specifier, context);
 
-	if (!specifier.endsWith('.scss')) return nextResult;
+  if (!specifier.endsWith('.scss')) return nextResult;
 
-	return {
-		format: 'scss',
-		shortCircuit: true,
-		url: nextResult.url,
-	};
+  return {
+    format: 'scss',
+    shortCircuit: true,
+    url: nextResult.url,
+  };
 }
 
 export async function load(url, context, next) {
-	if (context.format !== 'scss') return next(url, context);
+  if (context.format !== 'scss') return next(url, context);
 
-	return {
-		format: 'module',
-		shortCircuit: true,
-		source: '',
-	};
+  return {
+    format: 'module',
+    shortCircuit: true,
+    source: '',
+  };
 }
