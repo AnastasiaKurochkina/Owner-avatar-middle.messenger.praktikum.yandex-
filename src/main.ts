c8e49bd6cac4359d661e6b96b324b873967c3f73
Anastasia Kurochkina <kuronastya14@yandex.ru>
2024-02-21 21:30:48 +0400

    перенос роутера в main

--- src/main.ts
+++ src/main.ts
@@ -60,22 +60,15 @@ const pages = {
   [PAGES.not_found]: Pages.NotFound,
   [PAGES.server_error]: Pages.ServerError,
 };
-// const router = new Router('#app');
-
-// Object.entries(pages).forEach(([path, page]) => {
-//   router.use(path, page);
-// });
-// router.start();
 
 // eslint-disable-next-line import/no-mutable-exports
 export let router: Router;
 
-document.addEventListener('DOMContentLoaded', () => {
-  router = new Router('#app')
+document.addEventListener('DOMContentLoaded', async () => {
+  router = new Router('#app');
   Object.entries(pages).forEach(([path, page]) => {
-  router.use(path, page);
-});
+    router.use(path, page);
+  });
   router.start();
-  initApp();
+  await initApp();
 });
-
